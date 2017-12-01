3b167cdd79c71f5ce035f1ca8a0d9283b6e2af6e
Robert Hitchcock <roberthitchcock163@gmail.com>
2017-12-01 09:38:57 +0000

    Completed day 1

---
+++ day1/part2.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print("Usage: " + sys.argv[0] + " <code>")
+  else:
+    code = sys.argv[1]
+    total = 0
+    for i in range(0, len(code)):
+      if code[i] == code[int(i + len(code) / 2) % len(code)]:
+        total += int(code[i])
+    print(total)
+
