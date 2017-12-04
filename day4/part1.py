c2132335871d46062abe5b25deee61a45bb6df23
Robert Hitchcock <roberthitchcock163@gmail.com>
2017-12-04 05:21:37 +0000

    Completed day 4

---
+++ day4/part1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def is_valid(r):
+  for i in range(0, len(r)):
+    for j in range(i + 1, len(r)):
+      if r[i] == r[j]:
+        return 0
+  return 1
+
+rs = []
+with open("input.txt") as f:
+  for l in f.readlines():
+    r = l.split()
+    if r:
+      rs.append(r)
+t = 0
+for r in rs:
+  t += is_valid(r)
+print(t)
+
